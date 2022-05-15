c41c9aba491049104a5269562529efa2c245ecea
vinaysharmadev <shivimeenusharma@gmail.com>
2022-05-15 21:05:36 +0530

    some modifications

--- src/main/java/com/pwa/messenger/models/CustomMessage.java
+++ src/main/java/com/pwa/messenger/models/CustomMessage.java
@@ -1,18 +1,5 @@
 package com.pwa.messenger.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
-@AllArgsConstructor
-@Getter
-@Setter
 public class CustomMessage {
     boolean addMsg;
     int groupId;
@@ -21,7 +8,123 @@ public class CustomMessage {
     int status;
 
     long senderId;
+
+    public CustomMessage() {
+    }
+
+    public CustomMessage(boolean addMsg, int groupId, int msgType, int isMyself, int status, long senderId, String receiverId, long unixTimestamp, String first_name, String last_name, String senderName, Msg message) {
+        this.addMsg = addMsg;
+        this.groupId = groupId;
+        this.msgType = msgType;
+        this.isMyself = isMyself;
+        this.status = status;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.unixTimestamp = unixTimestamp;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.senderName = senderName;
+        this.message = message;
+    }
+
     String receiverId;
+
+    public boolean isAddMsg() {
+        return addMsg;
+    }
+
+    public void setAddMsg(boolean addMsg) {
+        this.addMsg = addMsg;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public int getMsgType() {
+        return msgType;
+    }
+
+    public void setMsgType(int msgType) {
+        this.msgType = msgType;
+    }
+
+    public int getIsMyself() {
+        return isMyself;
+    }
+
+    public void setIsMyself(int isMyself) {
+        this.isMyself = isMyself;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public long getUnixTimestamp() {
+        return unixTimestamp;
+    }
+
+    public void setUnixTimestamp(long unixTimestamp) {
+        this.unixTimestamp = unixTimestamp;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public Msg getMessage() {
+        return message;
+    }
+
+    public void setMessage(Msg message) {
+        this.message = message;
+    }
+
     long unixTimestamp;
 
 
--- src/main/java/com/pwa/messenger/models/Msg.java
+++ src/main/java/com/pwa/messenger/models/Msg.java
@@ -5,16 +5,86 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.Date;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Getter
-@Setter
-public class Msg{
+public class Msg {
+    public Msg() {
+    }
 
     long msgId;
     String senderName;
     int type;
+
+    public long getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(long msgId) {
+        this.msgId = msgId;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(int mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getCallerId() {
+        return callerId;
+    }
+
+    public void setCallerId(int callerId) {
+        this.callerId = callerId;
+    }
+
+    public int getIsGroupCall() {
+        return isGroupCall;
+    }
+
+    public void setIsGroupCall(int isGroupCall) {
+        this.isGroupCall = isGroupCall;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public long getUNIXTime() {
+        return UNIXTime;
+    }
+
+    public void setUNIXTime(long UNIXTime) {
+        this.UNIXTime = UNIXTime;
+    }
+
     int mediaType;
     String text;
     int callerId;
